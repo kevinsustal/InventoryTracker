6504db5731448d689d500c599bc1033ae61eb6c3
Kevin Sustal <kevinsustal@gmail.com>
2025-03-22 19:56:34 +0800

    Added product entity with Hibernate

---
+++ inventorytracker/src/main/java/com/example/inventorytracker/models/Product.java
@@ -0,0 +1,28 @@
+package com.example.inventorytracker.models;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "products")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private int quantity;
+    
+    @Column(nullable = false)
+    private double price;
+}
